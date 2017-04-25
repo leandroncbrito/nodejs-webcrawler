ddb05b6b58ffbc915ab4792a46f0a2820d6e8786
Leandro Brito <leandroncbrito@gmail.com>
2017-04-24 21:48:37 -0300

    Criação de servidor http

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const http = require('http');
 const dotenv = require('dotenv');
 const crawler = require('./app/core/crawler.js');
 const config = require('./config/settings.json');
@@ -7,6 +8,13 @@ const config = require('./config/settings.json');
 dotenv.load();
 
 let intervalo = config.minutes * 60 * 1000;
+
+http.createServer(function (req, res) {
+  
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Executando NodeJs WebCrawler!');
+
+}).listen(process.env.PORT || 8080);
 
 function iniciar(enviaremail) {
 
